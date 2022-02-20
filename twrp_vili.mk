4606a741967a9572b0771b7fad9698ca1b375eb4
Vikas <100078119+VIKAS9793@users.noreply.github.com>
2022-02-20 21:49:49 +0530

    Make way for vili
    Initial changes for Xiaomi 11T Pro

--- twrp_vili.mk
+++ twrp_vili.mk
@@ -14,12 +14,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/xiaomi/vili/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vili
-PRODUCT_NAME := omni_vili
+PRODUCT_NAME := twrp_vili
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := 2107113SC
+PRODUCT_MODEL := 2107113SI
 PRODUCT_MANUFACTURER := xiaomi
